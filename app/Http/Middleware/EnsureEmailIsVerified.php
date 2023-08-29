b8528f5ae9f56550d514aa0c17e4136238a31878
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-30 04:58:40 +0530

    Create EnsureEmailIsVerified.php

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $redirectToRoute
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
+     */
+    public function handle($request, Closure $next, $redirectToRoute = null)
+    {
+        if (! $request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+            ! $request->user()->hasVerifiedEmail())
+        ) {
+            $params = [];
+
+            if( env( 'SHOP_MULTILOCALE' ) ) {
+                $params['locale'] = $request->route( 'locale', $request->input( 'locale', app()->getLocale() ) );
+            }
+
+            if( env( 'SHOP_MULTISHOP' ) ) {
+                $params['site'] = $request->route( 'site', $request->input( 'site', config( 'shop.mshop.locale.site', 'default' ) ) );
+            }
+
+            return $request->expectsJson()
+                ? abort(403, 'Your email address is not verified.')
+                : Redirect::guest(airoute($redirectToRoute ?: 'verification.notice', $params));
+        }
+
+        return $next($request);
+    }
+}
