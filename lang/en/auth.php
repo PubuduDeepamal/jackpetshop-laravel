e8006ce3a632bdb79dd2b8690edc797f4ec2bf5a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-30 05:01:08 +0530

    Create auth.php

---
+++ lang/en/auth.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Login' => 'Login',
+    'E-Mail Address' => 'E-Mail Address',
+    'Password' => 'Password',
+    'Remember Me' => 'Remember Me',
+    'Forgot Password' => 'Forgot Password',
+
+    'Merchant registration' => 'Merchant registration',
+    'Registration' => 'Registration',
+    'Account' => 'Account',
+    'Confirm Password' => 'Confirm Password',
+    'Repeat password' => 'Repeat password',
+    'Register as merchant' => 'Register as merchant',
+    'Register' => 'Register',
+
+    'Verify Your Email Address' => 'Verify Your Email Address',
+    'A fresh verification link has been sent to your email address.' => 'A fresh verification link has been sent to your email address.',
+    'Before proceeding, please check your email for a verification link.' => 'Before proceeding, please check your email for a verification link.',
+    'Request another' => 'Request another',
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
